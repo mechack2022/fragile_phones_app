229f7cd3240902f7c04e9aabf65eaec63c333b08
taiwogboyegun <taiwogboyegunmechack@gmail.com>
2019-03-19 07:01:06 +0100

    nested array of object grabbed to keep the original copy

--- src/components/contextApi.js
+++ src/components/contextApi.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import {ProductStore, ProductDetails} from '../data'
-const ProductContext = React.createContext();
 
+const ProductContext = React.createContext();
 class ProductProvider extends Component {
     constructor(props){
         super(props)
         this.state={
-            ProductStore:ProductStore,
+            Products:[],
             ProductDetails:ProductDetails
         }
     }
+    // grab nested array of object to save the and set ot state origainal copy
+    setProduct = () =>{
+        let TempProducts = [];
+        ProductStore.forEach( item =>{
+            const singleItem = {...item }
+            TempProducts =[...TempProducts, singleItem]
+        })
+
+        this.setState({Products:TempProducts})
+
+
+    }
+    // react life cycle method
+    componentDidMount(){
+        this.setProduct()
+    }
+    
     // add to cart function
     addCart =() =>{
 
