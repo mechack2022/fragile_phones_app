169207a1553f5e9acad17c086df8b378f4afe90e
taiwogboyegun <taiwogboyegunmechack@gmail.com>
2019-03-19 21:41:16 +0100

    adding style to  cartProps using props as the advantage of styled components

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,21 @@ import styled from 'styled-components'
 
  export const ButtonContainer = styled.button`
     text-transform:capitalise;
-    color:var(--lightBlue);
+    color:${props => 
+        props.cartProp ? "var(--mainYellow)": "var(--lightBlue)" };
     font-size:1.4rem;
     background:transparent;
     padding:0.2rem 0.5rem;
     border-radius:0.5rem;
     border:0.05rem solid var(--lightBlue);
+    border-color:${props => 
+        props.cartProp ? "var(--mainYellow)": "var(--lightBlue)" };
     margin:0.5rem 0.2rem 0.5rem 0;
     cursor:pointer;
     transition:all 2s ease-in-out;
     &:hover{
-        background:var(--lightBlue);
+        background:${props => 
+        props.cartProp ? "var(--mainYellow)": "var(--lightBlue)" };
         color:var(--mainblue)
     }
     &:focus{
