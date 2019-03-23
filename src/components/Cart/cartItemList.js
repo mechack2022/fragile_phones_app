6559604d9e2bcdf7803d04fa855cea9423477ff1
taiwogboyegun <taiwogboyegunmechack@gmail.com>
2019-03-23 23:43:47 +0100

    change cardlistItem from react class component to react class function

--- src/components/Cart/cartItemList.js
+++ src/components/Cart/cartItemList.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+
+import React from 'react'
 import CartItem from './cartItem';
 
-class CartListItem extends Component {
-
-   render() {
-      return (
-         <div>
-            <h2>cart item list component</h2>
-            <CartItem/>
-         </div>
-      );
-   }
+export default function CartItemList({value}) {
+   const { cart } = value ;
+   console.log(cart, value)
+  return (
+    <div>
+       {cart.map( item =>{
+               return <CartItem key={item.id} item={item} value={value}  />
+            })}
+    </div>
+  )
 }
-
-export default CartListItem;
